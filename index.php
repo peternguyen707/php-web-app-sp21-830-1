06cef7b84823c94bc67d70f589e3b7ddc1efd53e
peternguyen707 <82463477+peternguyen707@users.noreply.github.com>
2021-06-03 09:26:32 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 if(isset($_GET['FirstName'])){ //data exists, show it
-  echo $_GET['FirstName'];
+  echo $_GET['FirstName'] . ' ' . $_GET['LastName'];
 } else{//no data, show form
     echo '
     <form action="">
-    Name: <input type="text"
-    name="FirstName" /><br />
+    First Name: <input type="text" name="FirstName" /><br />
+    Last Name: <input type="text" name="LastName" /><br />
     <input type="submit" />
     </form>
     ';
