a69b5f8d8d18f833e842f4f04c016f1ccac40fe9
Peter Nguyen <peternguyen707@gmail.com>
2021-06-03 09:43:33 -0700

    Adds var_dump() to index.php

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 if(isset($_GET['FirstName'])){ //data exists, show it
-  echo $_GET['FirstName'] . ' ' . $_GET['LastName'];
+  //echo $_GET['FirstName'] . ' ' . $_GET['LastName'];
+
+echo '<pre>';
+var_dump($_GET);
+echo '</pre>';
+
+
 } else{//no data, show form
     echo '
     <form action="">
